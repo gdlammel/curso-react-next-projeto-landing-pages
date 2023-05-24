f5b698e1b9c4cfbc81a3d1835ad8ddbc3baadc7e
Guilherme Duarte <gdlammel04@gmail.com>
2023-05-23 23:32:11 -0300

    update headind component

--- src/components/Heading/index.test.tsx
+++ src/components/Heading/index.test.tsx
@@ -22,10 +22,24 @@ describe("<Heading />", () => {
 		expect(heading).toHaveStyleRule("color", "#fff");
 	});
 
-	it("should render correct heading sizes", () => {
+	it("should render correct small size", () => {
 		const { getByRole } = renderTheme(<Heading size="small">Text</Heading>);
 		const heading = getByRole("heading", { name: "Text" });
 		expect(heading).toHaveStyleRule("font-size", theme.font.sizes.medium);
+	});
+
+	it("should render correct medium size", () => {
+		const { getByRole } = renderTheme(
+			<Heading size="medium">Text</Heading>
+		);
+		const heading = getByRole("heading", { name: "Text" });
+		expect(heading).toHaveStyleRule("font-size", theme.font.sizes.large);
+	});
+
+	it("should render correct big size", () => {
+		const { getByRole } = renderTheme(<Heading size="big">Text</Heading>);
+		const heading = getByRole("heading", { name: "Text" });
+		expect(heading).toHaveStyleRule("font-size", theme.font.sizes.xlarge);
 	});
 
 	it("should render with uppercase letters", () => {
@@ -38,5 +52,11 @@ describe("<Heading />", () => {
 		const { container } = renderTheme(<Heading as="h6">Text</Heading>);
 		const heading = container.querySelector("h6");
 		expect(heading?.tagName.toLowerCase()).toBe("h6");
+	});
+
+	it("should match snapshot", () => {
+		const { getByRole } = renderTheme(<Heading>Text</Heading>);
+		const heading = getByRole("heading");
+		expect(heading).toMatchSnapshot();
 	});
 });
