c200bd8ba4e414c06ac1d825549e1277abd2d841
Guilherme Duarte <gdlammel04@gmail.com>
2023-05-26 03:56:15 -0300

    add background

--- src/components/GridText/index.tsx
+++ src/components/GridText/index.tsx
@@ -4,6 +4,7 @@ import { Text } from "../Text";
 import * as Styled from "./styles";
 
 type Props = {
+	background?: boolean;
 	title: string;
 	description: string;
 	grid: {
@@ -12,18 +13,25 @@ type Props = {
 	}[];
 };
 
-export function GridText({ title, description, grid }: Props) {
+export function GridText({
+	title,
+	description,
+	grid,
+	background = false,
+}: Props) {
 	return (
-		<SectionBackground>
+		<SectionBackground background={background}>
 			<Styled.Container>
-				<Heading size="huge" uppercase>
+				<Heading size="huge" uppercase colordark={!background}>
 					{title}
 				</Heading>
 				<Text>{description}</Text>
 				<Styled.Grid>
 					{grid.map((element) => (
 						<Styled.GridElement key={element.title}>
-							<Heading size="medium">{element.title}</Heading>
+							<Heading size="medium" colordark={!background}>
+								{element.title}
+							</Heading>
 							<Text>{element.description}</Text>
 						</Styled.GridElement>
 					))}
